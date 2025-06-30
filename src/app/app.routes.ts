8f40d62c9ebca4aa4e14d7c225359616593a0d0c
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-06-30 11:14:33 +0100

    modify: change the tasks display from cards to full table
    Changing the task displayand adding the route hash based

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { TicketsComponent } from './pages/tickets/tickets.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProcessesComponent } from './pages/processes/processes.component';
+import { TicketViewComponent } from './pages/ticket-view/ticket-view.component';
 
 export const routes: Routes = [
     { path: 'tickets', component: TicketsComponent },
+    { path: 'tickets/:id', component: TicketViewComponent },
+    { path: 'process', component: ProcessesComponent },
     { path: '', component: HomeComponent },
 ];
